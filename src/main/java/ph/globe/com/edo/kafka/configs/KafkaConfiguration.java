b9d08387c0364834db934c607e4377dbb3a9a5c0
Timothy Kyle Chan <tkbchan24@gmail.com>
2021-08-11 14:56:16 +0800

    Minor Modifications

--- src/main/java/ph/globe/com/edo/kafka/configs/KafkaConfiguration.java
+++ src/main/java/ph/globe/com/edo/kafka/configs/KafkaConfiguration.java
@@ -1,65 +1,75 @@
 package ph.globe.com.edo.kafka.configs;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.*;
+import org.springframework.kafka.listener.adapter.RecordFilterStrategy;
 import org.springframework.kafka.support.serializer.JsonSerializer;
-import ph.globe.com.edo.kafka.model.User;
+import org.springframework.retry.support.RetryTemplate;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.function.Consumer;
-@EnableKafka
-@Configuration
+
 public class KafkaConfiguration {
     @Bean
-    public ProducerFactory producerFactory(){
+    public ProducerFactory<String, String> producerFactory() {
         Map<String, Object> props = new HashMap<>();
-
         props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, AppConfiguration.bootstrapServers);
+        props.put(ProducerConfig.RETRIES_CONFIG, 0);
+        props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
+        props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
+        props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
         props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
-        props.put(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true");
-        props.put(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "1");
-        props.put(ProducerConfig.RETRY_BACKOFF_MS_CONFIG, "1000");
-        props.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, "5000");
-        props.put(ProducerConfig.RETRIES_CONFIG,"3");
-        props.put(ProducerConfig.ACKS_CONFIG,"all");
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         return new DefaultKafkaProducerFactory<>(props);
     }
 
     @Bean
-    public ConsumerFactory consumerFactory(){
-        Map<String, Object> props = new HashMap<>();
+    public KafkaTemplate<String, String> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
 
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        return new DefaultKafkaConsumerFactory<>(consumerProperties());
+    }
+
+    @Bean
+    public Map<String, Object> consumerProperties() {
+        Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, AppConfiguration.bootstrapServers);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, AppConfiguration.groupid);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "s1pGroup");
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
+        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, 15000);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
-        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
-        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, "30000");
-        return new DefaultKafkaConsumerFactory<>(props);
+        return props;
     }
 
 
+
     @Bean
-    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory(){
-        ConcurrentKafkaListenerContainerFactory<String, String> factory = new ConcurrentKafkaListenerContainerFactory();
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(consumerFactory());
+        factory.setRetryTemplate(new RetryTemplate());
+        factory.setRecordFilterStrategy(new RecordFilterStrategy<String, String>() {
+
+            @Override
+            public boolean filter(ConsumerRecord<String, String> consumerRecord) {
+                return consumerRecord.value().equals("unwanted");
+            }
+
+        });
         return factory;
     }
 
-    @Bean
-    public KafkaTemplate<String, String> kafkaTemplate(){
-        return new KafkaTemplate<String, String>(producerFactory());
-    }
 
 }
--- src/main/java/ph/globe/com/edo/kafka/resource/UserResource.java
+++ src/main/java/ph/globe/com/edo/kafka/resource/UserResource.java
@@ -22,5 +22,6 @@ public class UserResource {
         kafkaTemplate.send(AppConfiguration.topic, message);
 
         return "Published Successfully";
+
     }
 }
