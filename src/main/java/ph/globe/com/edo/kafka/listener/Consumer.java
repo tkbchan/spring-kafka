d223547e3775f8b36b70058f877d5ffc93a174f6
Timothy Kyle Chan <tkbchan24@gmail.com>
2021-09-08 13:58:54 +0800

    added a dead letter listener, not yet tested

--- src/main/java/ph/globe/com/edo/kafka/listener/Consumer.java
+++ src/main/java/ph/globe/com/edo/kafka/listener/Consumer.java
@@ -3,6 +3,9 @@ package ph.globe.com.edo.kafka.listener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.KafkaHeaders;
@@ -17,12 +20,8 @@ public class Consumer {
     public static class Listener {
         public final CountDownLatch latch = new CountDownLatch(3);
         private final Logger logger = LoggerFactory.getLogger(Consumer.class);
+        private final TaskExecutor exec = new SimpleAsyncTaskExecutor();
 
-
-        public Listener() {
-
-        }
-        @Autowired
         @KafkaListener(topics = AppConfiguration.topic, groupId = AppConfiguration.groupid, containerFactory = "kafkaListenerContainerFactory")
         public void listen(@Payload String message, @Header(KafkaHeaders.RECEIVED_PARTITION_ID) int partition) {
             try{
@@ -31,11 +30,19 @@ public class Consumer {
                 }
                 System.out.println("Successfully Received: " + message + " (partition: " + partition + ")");
                 this.latch.countDown();
+
             } catch (Exception e){
                 System.out.println("Error in sending record");
+                this.exec.execute(() -> System.out.println("Hit Enter to terminate..."));
                 //System.out.println(e);
-                //e.printStackTrace();
+                e.printStackTrace();
             }
+        }
+
+        @KafkaListener(id = "dltGroup", topics = "topic1.DLT")
+        public void dltListen(String in) {
+            logger.info("Received from DLT: " + in);
+            this.exec.execute(() -> System.out.println("Hit Enter to terminate..."));
         }
 
 
